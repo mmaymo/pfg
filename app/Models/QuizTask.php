1da518ad16ac8b93c725a0105636799fd82c5093
carmenmaymo <mayen22@gmail.com>
2020-11-03 18:50:28 +0100

    add task and subclasses

---
+++ app/Models/QuizTask.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class QuizTask extends Task
+{
+    use HasFactory;
+}
---
+++ database/migrations/2020_11_03_174241_create_tasks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('course_id')->constrained();
+            $table->string('type');
+            $table->json('properties');
+            $table->timestamps();
+            $table->index(['id', 'type']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
