a91c7631e21a654d9ca3824f080f2f0ae59f8164
carmenmaymo <mayen22@gmail.com>
2020-11-07 17:26:01 +0100

    add user course progress table

---
+++ database/migrations/2020_11_07_155437_create_users_course_progress_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersCourseProgressTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_course_progress', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('course_id')->constrained();
+            $table->integer('points');
+            $table->integer('progress');
+            $table->timestamps();
+            $table->unique(['course_id', 'user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_course_progress');
+    }
+}
