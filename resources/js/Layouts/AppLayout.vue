b0a461307adddddfaab57e91a4f81e363bf5e24a
carmenmaymo <mayen22@gmail.com>
2020-11-21 20:36:53 +0100

    change route to cursos

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -14,8 +14,8 @@
 
                         <!-- Navigation Links -->
                         <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                            <jet-nav-link :href="route('dashboard')" :active="$page.currentRouteName == 'dashboard'">
-                                Cursos
+                            <jet-nav-link :href="route('courses.index')" :active="$page.currentRouteName == 'courses.index'">
+                                Mis Cursos
                             </jet-nav-link>
                         </div>
                     </div>
@@ -71,25 +71,6 @@
                                             Crear nuevo curso
                                         </jet-dropdown-link>
 
-                                        <div class="border-t border-gray-100"></div>
-
-                                        <!-- Team Switcher -->
-                                        <div class="block px-4 py-2 text-xs text-gray-400">
-                                            Cambiar de curso
-                                        </div>
-
-                                        <template v-for="team in $page.user.all_teams">
-                                            <form @submit.prevent="switchToTeam(team)">
-                                                <jet-dropdown-link as="button">
-                                                    <div class="flex items-center">
-                                                        <svg v-if="team.id == $page.user.current_team_id" class="mr-2 h-5 w-5 text-green-400" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                                                        <div>{{ team.name }}</div>
-                                                    </div>
-                                                </jet-dropdown-link>
-                                            </form>
-                                        </template>
-
-                                        <div class="border-t border-gray-100"></div>
                                     </template>
 
                                     <!-- Authentication -->
