16bb612b8ae7d07eaa89e908a22cb924e9272f4c
carmenmaymo <mayen22@gmail.com>
2020-11-28 16:44:09 +0100

    add logo and css tweak

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="min-h-screen bg-gray-100">
-        <nav class="bg-white border-b border-gray-100 fixed top-0 inset-x-0 z-40">
+        <nav class="bg-white border-b border-gray-100 fixed top-0 inset-x-0 z-40 h-16">
             <!-- Primary Navigation Menu -->
             <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div class="flex justify-between h-16">
                     <div class="flex">
                         <!-- Logo -->
                         <div class="flex-shrink-0 flex items-center">
-                            <inertia-link :href="route('dashboard')">
+                            <inertia-link :href="route('courses.index')">
                                 <jet-application-mark class="block h-9 w-auto" />
                             </inertia-link>
                         </div>
@@ -175,8 +175,10 @@
 
 
         <!-- Page Content -->
-        <main>
-            <slot></slot>
+        <main class="mt-16 relative">
+
+            <slot name="header"></slot><slot ></slot>
+
         </main>
 
         <!-- Modal Portal -->
