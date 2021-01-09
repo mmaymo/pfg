47ccf89c5edd4a44d847358a299e8c40ac7313e8
carmenmaymo <mmaymo2alumno.uned.es>
2021-01-09 12:24:07 +0100

    add chapter id to task table

--- database/migrations/2020_12_25_073800_create_tasks_table.php
+++ database/migrations/2020_12_25_073800_create_tasks_table.php
@@ -17,6 +17,7 @@ class CreateTasksTable extends Migration
             $table->id();
             $table->string('name');
             $table->foreignId('course_id')->constrained()->onDelete('cascade');
+            $table->foreignId('chapter_id')->constrained('tasks', 'id')->nullable();
             $table->string('type');
             $table->integer('points')->default(0);
             $table->json('properties')->nullable();
