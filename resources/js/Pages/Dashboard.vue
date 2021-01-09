a11ba875e67716538e164f1f3415fd6fc28abf79
carmenmaymo <mmaymo2alumno.uned.es>
2021-01-09 09:29:42 +0100

    style dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -4,16 +4,43 @@
             <div class="lg:flex -mx-6">
                 <div id="mainContentWrap"
                      class="min-h-screen w-full lg:static lg:max-h-full lg:overflow-visible lg:w-3/4 xl:w-4/5 z-0 mt-16">
-                    <div id="mainContent" class="grid grid-cols-3 gap-4">
+                    <div id="mainContent" class="grid grid-cols-1 gap-4">
                         <div v-if="$page.isTeacher">
-                            <ul v-for="course in $page.ownedCourses">
-                                <li>
-                                    {{course.name}}
-                                    <jet-button><a :href="route('courses.show', {'course':course.id})">Editar</a></jet-button>
-                                    <jet-button><a :href="route('courses.tasks.show', {'course':course.id, 'task':Object.keys(course.positionArray)[0]})">Ver</a></jet-button>
-                                </li>
-                            </ul>
-                            <div><jet-button><a :href="route('courses.create')">Crear Curso</a></jet-button></div>
+                            <jet-action-section class="">
+                                <template #title>
+                                    Tus cursos
+                                </template>
+
+                                <template #description>
+                                </template>
+                                <template #content>
+                                    <div class="space-y-6">
+                                        <ul class="flex items-center justify-between" v-for="course in $page.ownedCourses">
+                                            <li class="flex items-center">
+                                                <span class="ml-4">{{course.name}}</span>
+                                                <jet-button><a :href="route('courses.show', {'course':course.id})">Editar</a></jet-button>
+                                                <jet-button><a :href="route('courses.tasks.show', {'course':course.id, 'task':Object.keys(course.positionArray)[0]})">Ver</a></jet-button>
+                                            </li>
+
+                                        </ul>
+                                    </div>
+                                </template>
+                            </jet-action-section>
+                            <jet-section-border />
+                            <jet-action-section class="">
+                                <template #title>
+                                    Crea un nuevo curso
+                                </template>
+
+                                <template #description>
+                                </template>
+                                <template #content>
+                                    <div class="space-y-6">
+                                        <jet-button><a :href="route('courses.create')">Crear Curso</a></jet-button>
+                                    </div>
+                                </template>
+                            </jet-action-section>
+
                         </div>
                         <div v-else-if="$page.isEnrolled">
                             <ul v-for="course in $page.enrolledCourses">
@@ -40,11 +67,15 @@
 <script>
 import AppLayout from './../Layouts/AppLayout'
 import JetButton from '../Jetstream/Button'
+import JetActionSection from "../Jetstream/ActionSection";
+import JetSectionBorder from "../Jetstream/SectionBorder";
 
 export default {
     components: {
         AppLayout,
         JetButton,
+        JetActionSection,
+        JetSectionBorder,
     },
 
     data() {
