4cf2ab9cf6a6ed587e3ebc98c21f1e2009f6dd1f
carmenmaymo <mmaymo2alumno.uned.es>
2021-01-09 12:27:37 +0100

    apply common style

--- resources/js/Pages/Courses/Create.vue
+++ resources/js/Pages/Courses/Create.vue
@@ -1,15 +1,13 @@
 <template>
     <app-layout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight px-8 py-4">
                 Crear curso
             </h2>
         </template>
 
-        <div>
-            <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
-                <create-course-form />
-            </div>
+        <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+            <create-course-form/>
         </div>
     </app-layout>
 </template>
