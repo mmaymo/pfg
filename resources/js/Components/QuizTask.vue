d3cad4a1c0248428f119a9ab879f3706b3ab5c15
carmenmaymo <mayen22@gmail.com>
2021-02-08 18:26:07 +0100

    Add shuffle responses on quiz

--- resources/js/Components/QuizTask.vue
+++ resources/js/Components/QuizTask.vue
@@ -11,7 +11,7 @@
             <h2 class="uppercase my-2 p-4">Tu Respuesta</h2>
             <hr>
             <ul class="p-4">
-                <li class="mt-2" v-for="(response, answerIndex) in quiz.responses">
+                <li class="mt-2" v-for="(response, answerIndex) in shuffleResponsesArray(quiz.responses)">
                     <input type="radio" :id="answerIndex" :value="answerIndex" v-model="picked" ref="answer">
                     <label :for="answerIndex">{{response}}</label>
                 </li>
@@ -60,6 +60,13 @@
                     currentObj.correctAnswer = error;
                 });
             },
+            shuffleResponsesArray(responsesArray) {
+                for (let i = responsesArray.length - 1; i > 0; i--) {
+                    const j = Math.floor(Math.random() * (i + 1));
+                    [responsesArray[i], responsesArray[j]] = [responsesArray[j], responsesArray[i]];
+                }
+                return responsesArray;
+            }
         },
     }
 </script>
