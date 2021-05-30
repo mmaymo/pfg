ead640726c1205537a4ab8bb865794aad651cb20
carmenmaymo <mayen22@gmail.com>
2021-05-30 17:10:54 +0200

    add integration tests login

---
+++ tests/Browser/LoginTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class LoginTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $user = User::factory()->create([
+                                            'email' => 'user1@test.com',
+                                        ]);
+
+        $this->browse(function ($browser) use ($user) {
+            $browser->visit('/login')
+                ->type('email', $user->email)
+                ->type('password', 'password')
+                ->press('ENTRA')
+                ->assertPathIs('/dashboard');
+        });
+    }
+}
